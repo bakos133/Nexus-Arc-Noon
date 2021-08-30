59aecf65a7bf22545ba7bb4d79d0a970f01a4541
Rectum Tosser <bakos199133@gmail.com>
2021-08-30 15:11:37 -0400

    Nexus Arc Noon - Fixed
    For Noob!

---
+++ N-A-N D-O-R/Assets/GData/Scripts/Gravity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public float force = 0;
+    public float gConstant = 0;
+    public float m1 = 0;
+    public float m2 = 0;
+    public float r = 0;
+
+
+    
+}
